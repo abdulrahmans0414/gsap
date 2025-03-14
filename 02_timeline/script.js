a5ffb57c5367237d80fd6f470516d8f08b91388b
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-14 10:27:51 +0530

    feat: Add 02_timeline with GSAP timeline animations for boxes

---
+++ 02_timeline/script.js
@@ -0,0 +1,19 @@
+// Timeline
+var tl = gsap.timeline()
+
+tl.to("#box1", {
+    x: 1000,
+    duration: 1.5,
+    delay: 1,
+})
+
+tl.to("#box2", {
+    x: 1000,
+    duration: 1.5,
+})
+
+tl.to("#box3", {
+    x: 1000,
+    duration: 1.5,
+
+})
